d881754735ab0886be75bcc71739c00f1f290764
pass0585@gmail.com <pass0585@gmail.com>
2021-05-03 11:12:43 +0900

    주석 추가

--- ConsoleFirstApp/ConsoleFirstApp/Program.cs
+++ ConsoleFirstApp/ConsoleFirstApp/Program.cs
@@ -7,7 +7,8 @@ namespace ConsoleFirstApp
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!"); //gg
-            Console.WriteLine("gg");
+            Console.WriteLine("gg");//
         }
     }
 }
+ 
