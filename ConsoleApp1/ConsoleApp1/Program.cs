d507cf44b1f908dae24ad4c5e4223027597fd4bd
pass0585@gmail.com <pass0585@gmail.com>
2021-05-07 12:23:24 +0900

    ExceptionTest

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ExceptionTestApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("프로그램 시작");
+
+            int[] array = new int[5];
+            for (int i = 0; i <= 5; i++)
+            {
+                try
+                {
+                    array[i] = (i + 1);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"문제발생 {ex.Message}, 관리자에게 문의하세요 : Main(16~)");
+                }
+            }
+
+            Console.WriteLine("다른 로직 수행");
+            //  ...
+
+            Console.WriteLine("프로그램 종료");
+
+        }
+    }
+}
