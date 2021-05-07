6175f71f80a9e08c7b0882f1d9febdd90f687209
pass0585@gmail.com <pass0585@gmail.com>
2021-05-07 17:41:48 +0900

    winform

---
+++ ExceptionTestApp/EventHandlerCheckApp/Mainform.Designer.cs
@@ -0,0 +1,107 @@
+﻿
+namespace EventHandlerCheckApp
+{
+    partial class Mainform
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.TxtUserID = new System.Windows.Forms.TextBox();
+            this.TxtPassword = new System.Windows.Forms.TextBox();
+            this.BtnLogin = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 46);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "UserID";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 82);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(72, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Password";
+            // 
+            // TxtUserID
+            // 
+            this.TxtUserID.Location = new System.Drawing.Point(106, 39);
+            this.TxtUserID.Name = "TxtUserID";
+            this.TxtUserID.Size = new System.Drawing.Size(270, 27);
+            this.TxtUserID.TabIndex = 2;
+            // 
+            // TxtPassword
+            // 
+            this.TxtPassword.Location = new System.Drawing.Point(106, 75);
+            this.TxtPassword.Name = "TxtPassword";
+            this.TxtPassword.Size = new System.Drawing.Size(270, 27);
+            this.TxtPassword.TabIndex = 3;
+            // 
+            // BtnLogin
+            // 
+            this.BtnLogin.Location = new System.Drawing.Point(422, 25);
+            this.BtnLogin.Name = "BtnLogin";
+            this.BtnLogin.Size = new System.Drawing.Size(89, 93);
+            this.BtnLogin.TabIndex = 4;
+            this.BtnLogin.Text = "로그인";
+            this.BtnLogin.UseVisualStyleBackColor = true;
+            this.BtnLogin.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Mainform
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(539, 144);
+            this.Controls.Add(this.BtnLogin);
+            this.Controls.Add(this.TxtPassword);
+            this.Controls.Add(this.TxtUserID);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Mainform";
+            this.Text = "이벤트확인앱";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox TxtUserID;
+        private System.Windows.Forms.TextBox TxtPassword;
+        private System.Windows.Forms.Button BtnLogin;
+    }
+}
+
---
+++ ExceptionTestApp/EventHandlerCheckApp/Mainform.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EventHandlerCheckApp
+{
+    public partial class Mainform : Form
+    {
+        public Mainform()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var result = TxtUserID.Text + "\n" + TxtPassword.Text;
+            MessageBox.Show($"당신의 아이디와 패스워드는 {result}");
+        }
+
+        
+
+    }
+}
