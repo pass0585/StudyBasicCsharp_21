ffbfb07b949b71610c9879de4d2940c5fb0046d1
pass0585@gmail.com <pass0585@gmail.com>
2021-05-10 09:28:50 +0900

    210510__1

---
+++ DelegateChainApp/DelegateChainApp/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace DelegateChainApp
+{
+    delegate int Calculate(int a, int b);
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region
+
+            Calculate calc;
+
+            //람다식
+            calc = (a, b) => a + b;
+            Console.WriteLine($"a + b = {calc(3, 5)}");
+
+
+            //일반식
+            /*calc = delegate (int a, int b)
+            {
+                return a + b;
+            };*/
+        }
+    }
+}
